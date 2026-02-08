b64614da94fa3320d922d1d847595754060478e4
enoch B <henokt828@gmail.com>
2026-02-08 22:36:48 +0300

    Integrate user, subscription, and authentication routes into the Express application and establish MongoDB connection on server start

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express'
 import { PORT } from './config/env.js';
+import userRoutes from './routes/user.routes.js'
+import subscriptionRoutes from './routes/subscription.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import connectToDatabase from './database/mongodb.js';
 
 const app=express();
 
+app.use(express.json());
+
+//Routes
+app.use('api/v1/auth', authRoutes)
+app.use('api/v1/users', userRoutes);
+app.use('api/v1/subscriptions', subscriptionRoutes)
 
 
 app.get('/health',(req, res)=>{
@@ -11,6 +21,8 @@ app.get('/health',(req, res)=>{
 
 app.listen(PORT,()=>{
     console.log(`Subscription tracker App is running on http://localhost:${PORT}`)
+   connectToDatabase();
+
 })
 
 export default app;
